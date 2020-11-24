12407e4414a5a6785daf4d530643a1bcdc65ca7d
blangwell <b.langwell@outlook.com>
2020-11-23 23:06:06 -0800

    [admin] Add table to admin.ejs and add admin.css

--- server.js
+++ server.js
@@ -17,21 +17,16 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signup', (req, res) => {
-  db.User.create({firstName: 'test testington', email: 'actualemail@email.fart'})
+  db.User.create({firstName: 'Barent', email: 'barent@email.fart'})
   .then(created => res.status(201).send(created))
   .catch(err => res.status(503).send(err));
 })
 
 app.get('/admin', (req, res) => {
-  // context = {users: db.User.find()}
   db.User.find()
   .then(users => res.render('admin', {users}))
   .catch(err => console.log(err))
-  // res.render('admin', context)
 })
-
-// we need two routes 
-
 
 app.listen(API_PORT, () => {
   console.log(`🌽 listenin' on port ${API_PORT}`);
